1770a94e077a1770f1a58c01f748ce3d828a8375
Angus Alves <angustalves@gmail.com>
2018-05-06 17:15:57 -0400

    updated install.sh to force delete of tmp files

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cd /tmp
 git clone https://github.com/gussamer/dixdyoumeanthis.git
 sudo cp dixdyoumeanthis/dixdyoumeanthis.js /usr/local/bin/dx
 sudo chmod +x /usr/local/bin/dx
-rm -r dixdyoumeanthis
+rm -fr dixdyoumeanthis
